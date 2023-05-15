fb45de535f8bc30458d2b61c502e26250760910b
~mikhers <~miguel.morenovuniagustiniana.edu.co>
2023-05-15 03:24:22 -0500

    FORK Agrego metodo 'get' al archivo add_friends.dart

--- lib/screens/add_friends.dart
+++ lib/screens/add_friends.dart
@@ -1,23 +1,61 @@
 import "package:flutter/material.dart";
 import 'package:flutter/cupertino.dart';
 
-class AddFriends extends StatefulWidget {
+import '../models/models.dart';
+import '../services/usuario.service.dart';
+
+/*class AddFriends extends StatefulWidget {
+  const AddFriends({Key? key}) : super(key: key);*/
+class AddFriends extends StatelessWidget {
   const AddFriends({Key? key}) : super(key: key);
 
-  @override
-  State<AddFriends> createState() => _AddFriendsState();
 
-}
+  Future<List<Usuario>> obtenerUsuarios() async {
+    final service = usuarioService();
+    return await service.getUser();
+  }
+ // bool added = true;
 
-class _AddFriendsState extends State<AddFriends> {
-  bool added = true;
+  /*@override
+  State<AddFriends> createState() => _AddFriendsState();*/
+
+/*class _AddFriendsState extends State<AddFriends> {*/
   @override
   Widget build(BuildContext context) {
-    var mediaQuery = MediaQuery.of(context);
-    Size size = mediaQuery.size;
+    return FutureBuilder<List<Usuario>>(
+      future: obtenerUsuarios(),
+      builder: (BuildContext context, AsyncSnapshot<List<Usuario>> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          // Muestra un indicador de carga mientras se obtienen los datos
+          return CircularProgressIndicator();
+        } else if (snapshot.hasError) {
+          // Maneja cualquier error que se produzca durante la obtención de los datos
+          return Text('Error: ${snapshot.error}');
+        } else {
+          // Los datos se obtuvieron correctamente, puedes acceder a ellos en snapshot.data
+          List<Usuario> usuarios = snapshot.data!;
+          return Scaffold(
+            body: ListView.builder(
+              itemCount: usuarios.length,
+              itemBuilder: (BuildContext context, int index) {
+                Usuario usuario = usuarios[index];
+                return Card(
+                  child: ListTile(
+                    leading: Image.network(usuario.imagen ?? "https://static.vecteezy.com/system/resources/previews/008/442/086/original/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg"),
+                    title: Text(usuario.user_name),
+                  ),
+                );
+              },
+            ),
+          );
+        }
+      },
+    );
+  }
+
 
     
-    return Scaffold(
+    /*return Scaffold(
       body: Container(
         constraints: const BoxConstraints.expand(),
         // color: Colors.black,
@@ -168,6 +206,6 @@ class _AddFriendsState extends State<AddFriends> {
           ],
         ),
       ),
-    );
+    );*/
   }
-}
+//}
